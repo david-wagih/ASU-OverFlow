f224cf1dfe62550f8d8c9e385bb3142351ce22cb
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-04-28 16:40:10 +0200

    adding some Apis for creating and deleting users and adding some UI elements from Semantic UI

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -14,12 +14,14 @@ if (typeof window === "undefined") {
   if (process.env.NODE_ENV === "production") {
     prisma = new PrismaClient();
   } else {
+    // @ts-ignore
     if (!global.prisma) {
+      // @ts-ignore
       global.prisma = new PrismaClient();
     }
-
+    // @ts-ignore
     prisma = global.prisma;
   }
 }
-
+// @ts-ignore
 export default prisma;

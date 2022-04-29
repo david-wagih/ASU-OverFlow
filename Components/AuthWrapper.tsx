88a515bb032149771ea7e9e343317e4b0442fde0
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-04-29 23:17:49 +0200

    adding authentication using Github and protecting routes from unauthenticated users

---
+++ Components/AuthWrapper.tsx
@@ -0,0 +1,27 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React from "react";
+import ProtectedRoute from "./ProtectedRoute";
+
+// we can add here all the wanted routes to be protected
+const authRoutes = ["/dashboard"];
+
+const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      {authRoutes.includes(router.pathname) ? (
+        <ProtectedRoute> {children} </ProtectedRoute>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
+};
+
+export default AuthWrapper;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../Components/Header";
+import AuthWrapper from "../Components/AuthWrapper";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+      </AuthWrapper>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;

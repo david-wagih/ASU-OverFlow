bdae8fc14898a855b7e39af0656891ebb81b11a1
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-05-05 14:01:48 +0200

    adding list of user's asked questions in the profile page

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -1,15 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import Image from "next/image";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import { CategoriesIcons } from "../utils/CategoriesIcons";
+import { useRouter } from "next/router";
 
 // want to use user data from next-auth
 // @ts-ignore
 // we here could get all the user data needed from the session
 
-const ProfilePage = () => {
+const ProfilePage = (props: any) => {
   const { data, status } = useSession();
+  const router = useRouter();
 
   return (
     <Container className="d-flex justify-content-center mt-5">
@@ -20,8 +30,8 @@ const ProfilePage = () => {
             src={data.user.image}
             // @ts-ignore
             alt={data.user.name}
-            width={300}
-            height={300}
+            width={200}
+            height={200}
           />
         </Row>
         <br />
@@ -46,9 +56,61 @@ const ProfilePage = () => {
               : "You are not logged in"}
           </p>
         </Row>
+        <div>
+          <List>
+            {props.questions.map((question: any) => (
+              <ListItem
+                style={{
+                  cursor: "pointer",
+                  borderRadius: "10px",
+                  boxShadow: "0px 0px 2px #000000",
+                  marginTop: 20,
+                  backgroundColor: "#F5F5F5",
+                }}
+                onClick={() => {
+                  router.push("/QuestionsPage/" + question.id);
+                }}
+                key={question.id}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    variant="square"
+                    sx={{ width: 50, height: 50, marginRight: 5 }}
+                    src={
+                      question.category === "WebDevelopment"
+                        ? CategoriesIcons[0]
+                        : question.category === "MobileDevelopment"
+                        ? CategoriesIcons[1]
+                        : question.category === "DataScience"
+                        ? CategoriesIcons[2]
+                        : CategoriesIcons[3]
+                    }
+                  ></Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={question.content}
+                  secondary={question.createdAt}
+                ></ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
     </Container>
   );
 };
+export async function getServerSideProps(ctx: any) {
+  const session = await getSession(ctx);
+  const myQuestions = await fetch(
+    `http://localhost:3000/api/question/user/${session?.user?.email}`
+  );
+  const questions = await myQuestions.json();
+
+  return {
+    props: {
+      questions,
+    },
+  };
+}
 
 export default ProfilePage;

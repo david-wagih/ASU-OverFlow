c8f3e1045b037bb01b0a8cdc8d383cc2a7d7b2a9
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-05-11 12:38:39 +0200

    changing icon

--- utils/CategoriesIcons.ts
+++ utils/CategoriesIcons.ts
@@ -1,6 +1,6 @@
 export const CategoriesIcons = [
   "https://cdn-icons-png.flaticon.com/128/1336/1336494.png",
-  "https://cdn-icons.flaticon.com/png/128/5738/premium/5738077.png?token=exp=1651325437~hmac=c6e700a1572fb6eb876099b289cf0226",
+  "https://img.icons8.com/external-smashingstocks-glyph-smashing-stocks/344/external-mobile-development-digital-marketing-smashingstocks-glyph-smashing-stocks.png",
   "https://cdn-icons-png.flaticon.com/128/2329/2329087.png",
   "https://cdn-icons-png.flaticon.com/128/1693/1693746.png",
 ];
